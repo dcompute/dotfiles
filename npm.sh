32a3507a7c1eafd24afebc8876c779095c1fecde
Daniel Lacy <dan@daniellacy.com>
2016-02-19 12:05:22 -0500

    Add default NPM package list

---
+++ npm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install common NPM utilities
+npm install -g bower
+npm install -g eslint 
+npm install -g grunt-cli
+npm install -g istanbul
+npm install -g jsdoc-to-markdown
+npm install -g markdown-pdf
+npm install -g mocha
+npm install -g phantomjs
+npm install -g svgo
+
